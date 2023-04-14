812a231de7a97815e2c0a50b4655c4641cfffd19
APSODE <2gunbo74@gmail.com>
2023-04-15 01:18:05 +0900

    저능아 공부시키기 백준 문제 추가

---
+++ BaekjoonAssignment.h
@@ -0,0 +1,11 @@
+//
+// Created by leegu on 2023-04-15.
+//
+
+#ifndef DEBUG_TEST_BAEKJOONASSIGNMENT_H
+#define DEBUG_TEST_BAEKJOONASSIGNMENT_H
+
+int BaekjoonFindMaxAndMin(void);
+
+
+#endif //DEBUG_TEST_BAEKJOONASSIGNMENT_H
